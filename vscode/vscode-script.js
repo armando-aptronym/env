ece71eb102c994be9bb7fcf81a9c6651c0540154
Armando Gonzalez <armando.aptronym@gmail.com>
2024-11-19 12:22:07 -0500

    update

--- vscode/vscode-script.js
+++ vscode/vscode-script.js
@@ -78,4 +78,4 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-nodeRequire('electron').remote.getCurrentWindow().setVibrancy('ultra-dark');
+// nodeRequire('electron').remote.getCurrentWindow().setVibrancy('ultra-dark');
